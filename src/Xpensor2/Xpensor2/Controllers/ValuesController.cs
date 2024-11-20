51d39296272960e65e4894fea438b1ece865c827
Hiram Fernandes <hiram.sd@gmail.com>
2024-11-20 18:26:00 -0300

    Added controller

---
+++ src/Xpensor2/Xpensor2/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xpensor2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public async Task<IActionResult> Startup()
+        {
+            await Task.Delay(1000);
+
+            return Ok("Hello");
+        }
+    }
+}
