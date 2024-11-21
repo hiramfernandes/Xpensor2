4698ef56e6f27b606ea762f4107b4761dcfafa6f
Hiram Fernandes <hiram.sd@gmail.com>
2024-11-21 15:53:11 -0300

    Model updates

--- src/Xpensor2/Xpensor2.Domain/Models/Payment.cs
+++ src/Xpensor2/Xpensor2.Domain/Models/Payment.cs
@@ -2,10 +2,57 @@
 {
     public class Payment
     {
+        public Payment(string description, User owner)
+        {
+            Id = Guid.NewGuid();
+            Description = description;
+            Owner = owner;
+        }
+
         public Guid Id { get; set; }
+        public string Description { get; init; }
         public decimal? Value { get; set; }
+
+        public DateTime DueDate { get; set; }
+
         public DateTime Created { get; set; }
         public DateTime Modified { get; set; }
-        public string? Owner { get; set; }
+        public User? Owner { get; set; }
+    }
+
+    public class PaymentSlice
+    {
+        private List<Expenditure> _expenditures;
+
+        public PaymentSlice()
+        {
+            _expenditures = new List<Expenditure>();
+        }
+
+        public DateTime SliceStart { get; set; }
+        public DateTime SliceEnd { get; set; }
+        public IEnumerable<Expenditure>? Expenditures { get; set; }
+
+        public IEnumerable<Expenditure>? GenerateSlice(DateTime from, DateTime to)
+        {
+            return _expenditures.Where(x => x.DueDate >= from && x.DueDate <= to);
+        }
+    }
+
+    public class Expenditure
+    {
+        public string? Name { get; set; }
+        public DateTime DueDate { get; set;}
+    }
+
+    public class User
+    {
+        public Guid Id { get; init; }
+        public string? Name { get; set; }
+
+        public void CreatePayment(string description)
+        {
+            var payment = new Payment(description, this);
+        }
     }
 }
