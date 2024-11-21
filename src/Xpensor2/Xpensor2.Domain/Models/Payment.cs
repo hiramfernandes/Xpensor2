3537dee8f89d066017e32effbe0dd178a4a4fedf
Hiram Fernandes <hiram.sd@gmail.com>
2024-11-20 21:11:52 -0300

    Added domain project

---
+++ src/Xpensor2/Xpensor2.Domain/Models/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace Xpensor2.Domain.Models
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public decimal? Value { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public string? Owner { get; set; }
+    }
+}
