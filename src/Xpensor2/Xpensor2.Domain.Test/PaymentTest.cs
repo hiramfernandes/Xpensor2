5c0185ff394f6f337ee4337a17d47ee7730c7c69
Hiram Fernandes <hiram.sd@gmail.com>
2024-11-30 14:55:34 -0300

    Made payments tied to User

--- src/Xpensor2/Xpensor2.Domain.Test/PaymentTest.cs
+++ src/Xpensor2/Xpensor2.Domain.Test/PaymentTest.cs
@@ -1,38 +1,44 @@
 ﻿using FluentAssertions;
 using Xpensor2.Domain.Models;
 
-namespace Xpensor2.Domain.Test
+namespace Xpensor2.Domain.Test;
+
+public class PaymentTest
 {
-    public class PaymentTest
+    [Fact]
+    public void GenerateExpenses_ShouldCreateExpenses_BasedOnExistingPayments()
     {
-        [Fact]
-        public void GenerateExpenses_ShouldCreateExpenses_BasedOnExistingPayments()
-        {
-            var user = new User();
-            var pmtDescription = "CEEE";
-            var dueDay = 5;
-            var pmt = user.CreatePayment(description: pmtDescription, nominalValue: 150, dueDay: dueDay);
-            var pmt2 = user.CreatePayment(description: "Pmt2", nominalValue: 100, dueDay: 2);
-            var pmt3 = user.CreatePayment(description: "Pmt3", nominalValue: 200, dueDay: 5);
-            var pmt4 = user.CreatePayment(description: "Pmt4", nominalValue: 300, dueDay: 10);
+        // Arrange
+        var user = new User(Guid.NewGuid(), "name");
+        var pmtDescription = "CEEE";
+        var dueDay = 5;
+        var pmt = user.CreatePayment(description: pmtDescription, nominalValue: 150, dueDay: dueDay);
+        var pmt2 = user.CreatePayment(description: "Pmt2", nominalValue: 100, dueDay: 2);
+        var pmt3 = user.CreatePayment(description: "Pmt3", nominalValue: 200, dueDay: 5);
+        var pmt4 = user.CreatePayment(description: "Pmt4", nominalValue: 300, dueDay: 10);
 
-            var slice = new PaymentSlice();
-            slice.AddPayment(pmt);
-            slice.AddPayment(pmt2);
-            slice.AddPayment(pmt3);
-            slice.AddPayment(pmt4);
-            var referenceDate = DateTime.Today;
-            pmt.DueDate = referenceDate;
-            var expenditures = slice.GenerateExpenditures(referenceDate);
+        var slice = new PaymentSlice();
+        slice.AddPayment(pmt);
+        slice.AddPayment(pmt2);
+        slice.AddPayment(pmt3);
+        slice.AddPayment(pmt4);
+        var referenceDate = DateTime.Today;
+        pmt.DueDate = referenceDate;
 
-            expenditures.Should().NotBeNull();
-            expenditures.Should().NotBeEmpty();
-            expenditures.Should().HaveCount(4);
+        // Act
+        var expenditures = slice.GenerateExpenditures(referenceDate);
 
-            expenditures!.First().Name.Should().Be(pmtDescription);
-            expenditures!.First().DueDate.Day.Should().Be(dueDay);
-            expenditures!.First().DueDate.Month.Should().Be(referenceDate.Month);
-            expenditures!.First().DueDate.Year.Should().Be(referenceDate.Year);
-        }
+        // Assert
+        user.Payments.Should().NotBeNull();
+        user.Payments.Should().HaveCount(4);
+
+        expenditures.Should().NotBeNull();
+        expenditures.Should().NotBeEmpty();
+        expenditures.Should().HaveCount(4);
+
+        expenditures!.First().Name.Should().Be(pmtDescription);
+        expenditures!.First().DueDate.Day.Should().Be(dueDay);
+        expenditures!.First().DueDate.Month.Should().Be(referenceDate.Month);
+        expenditures!.First().DueDate.Year.Should().Be(referenceDate.Year);
     }
 }
--- src/Xpensor2/Xpensor2.Domain/Models/Payment.cs
+++ src/Xpensor2/Xpensor2.Domain/Models/Payment.cs
@@ -1,80 +1,122 @@
-﻿namespace Xpensor2.Domain.Models
+﻿namespace Xpensor2.Domain.Models;
+
+public class Payment
 {
-    public class Payment
+    public Payment(string description, User owner, decimal nominalValue, int dueDay)
     {
-        public Payment(string description, User owner, decimal nominalValue, int dueDay)
-        {
-            Id = Guid.NewGuid();
-            Description = description;
-            Owner = owner;
-            NominalValue = nominalValue;
-            DueDay = dueDay;
-            Recurrence = PaymentRecurrence.Monthly;
-        }
-
-        public Guid Id { get; set; }
-        public string Description { get; init; }
-        public decimal? NominalValue { get; set; }
-        public DateTime DueDate { get; set; }
-        public int DueDay { get; set; }
-        public PaymentRecurrence Recurrence { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Modified { get; set; }
-        public User? Owner { get; set; }
+        Id = Guid.NewGuid();
+        Description = description;
+        Owner = owner;
+        NominalValue = nominalValue;
+        DueDay = dueDay;
+        Recurrence = PaymentRecurrence.Monthly;
     }
 
-    public enum PaymentRecurrence
+    public Guid Id { get; private set; }
+    public string Description { get; init; }
+    public decimal? NominalValue { get; set; }
+    public DateTime DueDate { get; set; }
+    public int DueDay { get; set; }
+    public PaymentRecurrence Recurrence { get; set; }
+    public IList<ExecutedPayment> ExecutedPayments { get; private set; } = [];
+    public DateTime Created { get; set; }
+    public DateTime Modified { get; set; }
+    public User? Owner { get; set; }
+
+
+    public void AddExecutedPayment(ExecutedPayment executedPayment)
     {
-        Monthly
+        ExecutedPayments.Add(executedPayment);
+    }
+}
+
+public enum PaymentRecurrence
+{
+    Monthly
+}
+
+public class ExecutedPayment
+{
+    public required Payment Payment { get; set; }
+    public required string PaymentMethod { get; set; }
+    public required DateTime PaidDate { get; set; }
+    public required User PaidBy { get; set; }
+}
+
+public class PaymentSlice
+{
+    public PaymentSlice() => Payments = [];
+
+    public List<Payment>? Payments { get; private set; }
+
+    public void AddPayment(Payment payment)
+    {
+        Payments!.Add(payment);
     }
 
-    public class PaymentSlice
+    public IEnumerable<Expenditure>? GenerateExpenditures(DateTime referenceDate)
     {
-        public PaymentSlice() => Payments = [];
-
-        public List<Payment>? Payments { get; private set; }
-
-        public void AddPayment(Payment payment)
-        {
-            Payments!.Add(payment);
-        }
-
-        public IEnumerable<Expenditure>? GenerateExpenditures(DateTime referenceDate)
-        {
-            // Regular Payments (no end date)
-            return Payments!
-                .Select(pmt => MapFrom(payment: pmt,
-                                       month: referenceDate.Month,
-                                       year: referenceDate.Year));
-        }
-
-        private static Expenditure MapFrom(Payment payment, int month, int year)
-        {
-            return new Expenditure()
-            {
-                DueDate = new DateTime(year, month, payment.DueDay),
-                Name = payment.Description,
-                GeneralInfo = string.Empty
-            };
-        }
+        // Regular Payments (no end date)
+        return Payments!
+            .Select(pmt => MapFrom(payment: pmt,
+                                   month: referenceDate.Month,
+                                   year: referenceDate.Year));
     }
 
-    public class Expenditure
+    private static Expenditure MapFrom(Payment payment, int month, int year)
     {
-        public string? Name { get; set; }
-        public DateTime DueDate { get; set; }
-        public string? GeneralInfo { get; set; }
-        public DateTime? PaymentDate { get; set; }
+        return new Expenditure()
+        {
+            DueDate = new DateTime(year, month, payment.DueDay),
+            Name = payment.Description,
+            GeneralInfo = string.Empty
+        };
+    }
+}
+
+public class Expenditure
+{
+    public string? Name { get; set; }
+    public DateTime DueDate { get; set; }
+    public string? GeneralInfo { get; set; }
+    public DateTime? PaymentDate { get; set; }
+}
+
+public class User
+{
+    public User(Guid id, string? name)
+    {
+        Id = id;
+        Name = name;
     }
 
-    public class User
-    {
-        public Guid Id { get; init; }
-        public string? Name { get; set; }
+    public Guid Id { get; init; }
+    public string? Name { get; init; }
+    public IList<Payment> Payments { get; private set; } = [];
 
-        public Payment CreatePayment(string description, decimal nominalValue, int dueDay)
+    public Payment CreatePayment(string description, decimal nominalValue, int dueDay)
+    {
+        var newPayment = new Payment(description, this, nominalValue, dueDay);
+        Payments.Add(newPayment);
+
+        return newPayment;
+    }
+
+    public Payment? GetPayment(Guid paymentId)
+    {
+        return Payments.FirstOrDefault(x => x.Id == paymentId);
+    }
+
+    public bool RegisterExecutedPayment(Payment payment, ExecutedPayment executedPayment)
+    {
+        var selectedPmt = GetPayment(payment.Id);
+        if (selectedPmt == null)
         {
-            return new Payment(description, this, nominalValue, dueDay);
+            return false;
         }
+
+        selectedPmt!.AddExecutedPayment(executedPayment);
+
+        return true;
     }
 }
